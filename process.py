fbc8f432414565cfc86902985e98e353e1727ffd
safisher <safisher@users.noreply.github.com>
2021-11-19 22:14:44 +0000

    included genes and proteins and minor updates
    Genes and proteins are handled differently from structures and cells.
    Updated the code to better conform to Python conventions.

--- process.py
+++ process.py
@@ -5,106 +5,155 @@ import re
 
 DEBUG = False
 
-ASLevels = 10
-CTLevels = 3
-BGLevels = 10
+# XXX need to fix output of gene and protein labels and ID's
 
-asDict = {}
+# This should be an argument
+#HEAD = "ovary"
+HEAD = "head"
 
-def outputStruct(parents):
+# these need to be manually set based on the organ. They should probably be arguments.
+AS_LEVELS = 3
+CT_LEVELS = 3
+BGene_LEVELS = 3
+BProtein_LEVELS = 2
+
+input_dict = {}
+
+
+def output_struct(parents):
     # we want to go from the top level structure to the bottom level
     parents.reverse()
 
     level = 0
     for key in parents:
-        if DEBUG: print(key, end="\t")
-        
-        sType = asDict[key]['sType']
-        if sType == "AS":
+        if DEBUG:
+            print(key, end="\t")
+
+        s_type = input_dict[key]['s_type']
+        if s_type == "AS":
             # process anatomical structures
             if level == 0:
-                outString = key + "\t" + asDict[key]['label'] + "\t" + asDict[key]['id']
+                out_string = key + "\t" + input_dict[key]['label'] + "\t" + input_dict[key]['id']
             else:
-                outString = "\t" + key + "\t" + asDict[key]['label'] + "\t" + asDict[key]['id']
+                out_string = "\t" + key + "\t" + input_dict[key]['label'] + "\t" + input_dict[key]['id']
             level += 1
 
-        elif sType == "CT":
+        elif s_type == "CT":
             # process cell types
-            outString = ""
-            while level < ASLevels:
-                outString += "\t\t\t"
+            out_string = ""
+            while level < AS_LEVELS:
+                out_string += "\t\t\t"
                 level += 1
-            outString += "\t" + key + "\t" + asDict[key]['label'] + "\t" + asDict[key]['id']
-            level += 1
-                
-        elif sType == "BG":
-            # process genes
-            outString = ""
-            while level < (ASLevels + CTLevels):
-                outString += "\t\t\t"
-                level += 1
-            outString += "\t" + key + "\t" + asDict[key]['label'] + "\t" + asDict[key]['id']
+            out_string += "\t" + key + "\t" + input_dict[key]['label'] + "\t" + input_dict[key]['id']
             level += 1
 
-        # outFile.write(key + "\t" + asDict[key]['label'] + "\t" + asDict[key]['id'])
-        outFile.write(outString)
-                
-    if DEBUG: print()
-    outFile.write("\n")
+        else:
+            # children should only be of type AS or CT
+            print("ERROR: erroneous child value", input_dict[key])
+            continue
+
+        genes = input_dict[key]['genes']
+        if genes:
+            # process genes
+            while level < (AS_LEVELS + CT_LEVELS):
+                out_string += "\t\t\t"
+                level += 1
+
+            # XXXXX NEED to extract label and id for each gene and output them.
+            # currently we're pulling the "key" label and id not the "gene" label and id
+            for gene in genes:
+                out_string += "\t" + gene + "\t" + input_dict[key]['label'] + "\t" + input_dict[key]['id']
+                level += 1
+
+        proteins = input_dict[key]['proteins']
+        if proteins:
+            # process proteins
+            while level < (AS_LEVELS + CT_LEVELS + BGene_LEVELS):
+                out_string += "\t\t\t"
+                level += 1
+
+            # XXXXX NEED to extract label and id for each protein and output them.
+            # currently we're pulling the "key" label and id not the "protein" label and id
+            for protein in proteins:
+                out_string += "\t" + protein + "\t" + input_dict[key]['label'] + "\t" + input_dict[key]['id']
+                level += 1
+
+        out_file.write(out_string)
+
+    if DEBUG:
+        print()
+    out_file.write("\n")
 
     # restore list order
     parents.reverse()
 
+
 # this is very inefficient but optimized for easy inputs and assumed processing speed is irrelevant.
-def getParents(leaf, parents):
+def get_parents(leaf, parents):
     # loop through all stuctures and print any that contain this leaf as a child
-    for key in asDict.keys():
-        children = asDict[key]["children"]
+    for key in input_dict.keys():
+        children = input_dict[key]["children"]
         if not children:
             continue
         if leaf in children:
             parents.append(key)
-            getParents(key, parents)
+            get_parents(key, parents)
 
     # if at the top level, then output the list
-    if leaf == "ovary": outputStruct(parents)
+    if leaf == HEAD:
+        output_struct(parents)
 
     # remove latest structure as we back up the recursion.
     parents.pop()
-            
+
+
 # execute script
 if __name__ == "__main__":
     # open input file. argv[0] is the program name
-    inFilename = sys.argv[1]
-    inFile = open(inFilename, "r")
-    
+    in_filename = sys.argv[1]
+    in_file = open(in_filename, "r")
+
     # this will overwrite any existing file
-    outFilename = sys.argv[2]
-    outFile = open(outFilename, "w")
-    
-    contents = inFile.readlines()
+    out_filename = sys.argv[2]
+    out_file = open(out_filename, "w")
+
+    contents = in_file.readlines()
     for line in contents:
         # the first line might be a header line. That's intrisically resolved as the header "shouldn't" match to any children
-        name, label, reference, sType, children = re.split(r'\t', line.rstrip('\n'))
+        name, label, reference, s_type, children_string, genes_string, proteins_string = re.split(r'\t', line.rstrip('\n'))
 
-        # remove the quotes from the TSV file and convert from a string into a list
-        if children:
-            # doing the replace as a separate step to allow for ', ' and ',' in the source file
-            children = children.replace('"','').replace(', ',',')
-            kids = children.split(',')
-        else:
-            kids = []
-            
+        # skip header line
+        if name.lower() == "name":
+            continue
+
+        # remove the quotes and extra spaces from the TSV file
+        children_string = children_string.replace('"', '').replace(', ', ',')
+        genes_string = genes_string.replace('"', '').replace(', ', ',')
+        proteins_string = proteins_string.replace('"', '').replace(', ', ',')
+
+        # convert from a string into a list
+        children = []
+        if children_string:
+            children = children_string.split(',')
+        genes = []
+        if genes_string:
+            genes = genes_string.split(',')
+        proteins = []
+        if proteins_string:
+            proteins = proteins_string.split(',')
+
         # add anatomical structure to our dictionary
-        asDict.update({name:{"label":label,"id":reference,"sType":sType,"children":kids}})
+        input_dict.update({name:{"label":label, "id":reference, "s_type":s_type, "children":children, "genes":genes, "proteins":proteins}})
 
     # all structures loaded so now we step though each structure and print out all leaves
-    for key in asDict.keys():
-        children = asDict[key]["children"]
-        if not children:
+    for key in input_dict.keys():
+        children = input_dict[key]["children"]
+        s_type = input_dict[key]['s_type']
+        # build hierarchical structure with anatomical structures and cells.
+        if not children and (s_type in ("AS", "CT")):
             # no children so end point and need to print upstream structures
             parents = [key]
-            getParents(key, parents)
-            
-    inFile.close()
-    outFile.close()
+            get_parents(key, parents)
+
+    in_file.close()
+    out_file.close()

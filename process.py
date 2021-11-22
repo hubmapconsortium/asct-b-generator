394653495ef87164c76ba2f6bbc251c139fdb6c1
safisher <safisher@users.noreply.github.com>
2021-11-22 22:23:34 +0000

    added other field types

--- process.py
+++ process.py
@@ -17,6 +17,15 @@ CT_LEVELS = 1
 # compute these based on the max number of elements across cells
 BGene_levels = 0
 BProtein_levels = 0
+BProteoform_levels = 0
+BLipid_levels = 0
+BMetabolites_levels = 0
+# we assume FTU has the same three columns as the other descriptors.
+FTU_levels = 0
+# we don't actually need the REF levels since REF is the last set,
+# however, it's a hack to allow us to handle REF like the other
+# descriptors.
+REF_levels = 0
 
 input_dict = {}
 
@@ -67,8 +76,20 @@ def output_struct(parents):
             continue
 
         out_file.write(out_string)
-        level = print_details(input_dict[key]['genes'], (AS_LEVELS + CT_LEVELS), level)
-        level = print_details(input_dict[key]['proteins'], (AS_LEVELS + CT_LEVELS + BGene_levels), level)
+        level = print_details(input_dict[key]['genes'],
+                              (AS_LEVELS + CT_LEVELS), level)
+        level = print_details(input_dict[key]['proteins'],
+                              (AS_LEVELS + CT_LEVELS + BGene_levels), level)
+        level = print_details(input_dict[key]['proteoforms'],
+                              (AS_LEVELS + CT_LEVELS + BGene_levels + BProtein_levels), level)
+        level = print_details(input_dict[key]['lipids'],
+                              (AS_LEVELS + CT_LEVELS + BGene_levels + BProtein_levels + BProteoform_levels), level)
+        level = print_details(input_dict[key]['metabolites'],
+                              (AS_LEVELS + CT_LEVELS + BGene_levels + BProtein_levels + BProteoform_levels + BLipid_levels), level)
+        level = print_details(input_dict[key]['ftu'],
+                              (AS_LEVELS + CT_LEVELS + BGene_levels + BProtein_levels + BProteoform_levels + BLipid_levels + BMetabolites_levels), level)
+        level = print_details(input_dict[key]['refs'],
+                              (AS_LEVELS + CT_LEVELS + BGene_levels + BProtein_levels + BProteoform_levels + BLipid_levels + BMetabolites_levels + FTU_levels), level)
 
     if DEBUG:
         print()
@@ -97,6 +118,16 @@ def get_parents(leaf, parents):
     parents.pop()
 
 
+def split_string(array_string, max_level):
+    tmp = []
+    if array_string:
+        tmp = array_string.split(',')
+        if max_level < len(tmp):
+            max_level = len(tmp)
+
+    return tmp, max_level
+
+
 # execute script
 if __name__ == "__main__":
     # open input file. argv[0] is the program name
@@ -110,7 +141,7 @@ if __name__ == "__main__":
     contents = in_file.readlines()
     for line in contents:
         # the first line might be a header line. That's intrisically resolved as the header "shouldn't" match to any children
-        name, label, reference, s_type, children_string, genes_string, proteins_string = re.split(r'\t', line.rstrip('\n'))
+        name, label, reference, s_type, children_string, genes_string, proteins_string, proteoforms_string, lipids_string, metabolites_string, ftu_string, refs_string = re.split(r'\t', line.rstrip('\n'))
 
         # skip header line
         if name.lower() == "name":
@@ -120,26 +151,29 @@ if __name__ == "__main__":
         children_string = children_string.replace('"', '').replace(', ', ',')
         genes_string = genes_string.replace('"', '').replace(', ', ',')
         proteins_string = proteins_string.replace('"', '').replace(', ', ',')
+        proteoforms_string = proteoforms_string.replace('"', '').replace(', ', ',')
+        lipids_string = lipids_string.replace('"', '').replace(', ', ',')
+        metabolites_string = metabolites_string.replace('"', '').replace(', ', ',')
+        ftu_string = ftu_string.replace('"', '').replace(', ', ',')
+        refs_string = refs_string.replace('"', '').replace(', ', ',')
 
         # convert from a string into a list
         children = []
         if children_string:
             children = children_string.split(',')
-            
-        genes = []
-        if genes_string:
-            genes = genes_string.split(',')
-            if BGene_levels < len(genes):
-                BGene_levels = len(genes)
-                
-        proteins = []
-        if proteins_string:
-            proteins = proteins_string.split(',')
-            if BProtein_levels < len(proteins):
-                BProtein_levels = len(proteins)
 
+        # process descriptor arrays
+        genes, BGene_levels = split_string(genes_string, BGene_levels)
+        proteins, BProtein_levels = split_string(proteins_string, BProtein_levels)
+        proteoforms, BProteoform_levels = split_string(proteoforms_string, BProteoform_levels)
+        lipids, BLipid_levels = split_string(lipids_string, BLipid_levels)
+        metabolites, BMetabolites_levels = split_string(metabolites_string, BMetabolites_levels)
+        ftu, FTU_levels = split_string(ftu_string, FTU_levels)
+        refs, REF_levels = split_string(refs_string, REF_levels)
+
+        
         # add anatomical structure to our dictionary
-        input_dict.update({name:{"label":label, "id":reference, "s_type":s_type, "children":children, "genes":genes, "proteins":proteins}})
+        input_dict.update({name:{"label":label, "id":reference, "s_type":s_type, "children":children, "genes":genes, "proteins":proteins, "proteoforms":proteoforms, "lipids":lipids, "metabolites":metabolites, "ftu":ftu, "refs":refs }})
 
     # all structures loaded so now we step though each structure and print out all leaves
     for key in input_dict.keys():
